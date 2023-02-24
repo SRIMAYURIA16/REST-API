a9affb8416f4384f9c24d3bfce274af1c3247df1
SRIMAYURI <srimayuri16@gmail.com>
2023-02-24 12:17:13 +0530

    rest api

---
+++ day3/src/main/java/com/example/demo/model/Customer.java
@@ -0,0 +1,31 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+	@Id
+private int id;
+private String name;
+private int Age;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getAge() {
+	return Age;
+}
+public void setAge(int age) {
+	Age = age;
+}
+
+}
